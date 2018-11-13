79d6f6bb59946d969fe66258995d87b196a1f283
James Horton <jameshorton@gmail.com>
2018-11-13 16:46:52 -0500

    Updates

---
+++ precache-manifest.d00e900c151784b6cac300c2ada49ca4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "814e4ab6daea8bcbea46",
+    "url": "/tic-tac-toe-React/static/js/runtime~main.814e4ab6.js"
+  },
+  {
+    "revision": "fa026963d2e8a4aa9329",
+    "url": "/tic-tac-toe-React/static/js/main.fa026963.chunk.js"
+  },
+  {
+    "revision": "07861e9d5e216d6b0c3e",
+    "url": "/tic-tac-toe-React/static/js/1.07861e9d.chunk.js"
+  },
+  {
+    "revision": "fa026963d2e8a4aa9329",
+    "url": "/tic-tac-toe-React/static/css/main.4b9bc698.chunk.css"
+  },
+  {
+    "revision": "87b782f6b52edc5ce0fe8670a29ada04",
+    "url": "/tic-tac-toe-React/index.html"
+  }
+];
